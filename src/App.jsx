65990fd65a2c27cfeffe50a597f3e34aff5fd245
m-honarzade <honarzadeh.mahdie@gmail.com>
2024-05-02 17:55:02 +0330

    making appLayout

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Cabins from "./pages/Cabins";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
+import AppLayout from "./ui/AppLayout";
 
 const StyledApp = styled.main`
   padding: 20px;
@@ -21,14 +22,16 @@ const App = () => {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookins" element={<Bookings />} />
-          <Route path="account" element={<Account />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="account" element={<Account />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
           <Route path="login" element={<Login />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledSidebar = styled.aside`
+  padding: 3.2rem 2.4rem;
+  border-bottom: 1px solid var(--color-grey-100);
+  grid-row: 1/-1;
+`;
+const Sidebar = () => {
+  return <StyledSidebar>Sidebar</StyledSidebar>;
+};
+
+export default Sidebar;
